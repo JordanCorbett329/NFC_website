cd0c618962b8465e9b01bf226a7950eb08884d24
JordanCorbett329 <jordan.n.corbett@gmail.com>
2026-02-25 12:54:10 +1000

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  const btn = document.querySelector('[data-menu-btn]');
-  const drawer = document.querySelector('[data-mobile-drawer]');
-  if(!btn || !drawer) return;
-  btn.addEventListener('click', () => {
-    const isOpen = drawer.classList.toggle('open');
-    btn.setAttribute('aria-expanded', String(isOpen));
-  });
-})();
